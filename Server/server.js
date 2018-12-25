f5ebd46673ebb003420370e1dcd681800728d762
Mrityunjay <17mrityunjaypandey@gmail.com>
2018-12-25 18:48:00 +0530

    404 message add

--- Server/server.js
+++ Server/server.js
@@ -5,4 +5,9 @@ const product=require("./../Routes/products");
 var app=express();
 app.use(bodyParser.json());
 app.use(product);
+
+app.get("*",(req,res)=>{
+    res.send('404 Not Found');
+})
+
 module.exports=app;
